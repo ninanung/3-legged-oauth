08c24583a8bd6a7bc15ae8508bf66ef65f21e148
Seungje Jun <ninanung@naver.com>
2019-02-15 14:41:31 +0900

    divide the frontend and backend

--- server/app.js
+++ server/app.js
@@ -1,47 +1,19 @@
 var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
-var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 const cors = require('cors');
-
-const auth = require('./API/auth');
-const get = require('./API/get');
-const index = require('./API/index');
-const sign = require('./API/sign');
-
 const connectHistoryApiFallback = require('connect-history-api-fallback');
-const bodyParser = require('body-parser');
-
-const session = require('express-session');
-const FileStore = require('session-file-store')(session);
 
 const app = express();
 
-app.use(cors({origin:"http://localhost:3002"}));
-app.use(session({
-  secret: 'some secret',  // 암호화
-  resave: false,
-  saveUninitialized: true,
-  store: new FileStore()
-}));
-
-// view engine setup
+app.use(cors({origin:"http://localhost:3000"}));
 app.set('view engine', 'html');
-
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(bodyParser.json());
 app.use(connectHistoryApiFallback());
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'build')));
-
-app.use('/', index);
-app.use('/api/auth', auth);
-app.use('/api/get', get);
-app.use('/api/sign', sign);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
